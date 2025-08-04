fbd6fa934481316f2549072fdd09cb39a8a4263e
Can Akgül <can.akgul@metu.edu.tr>
2025-08-04 15:40:36 +0300

    Sıfırlandı

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -1,9 +1,1 @@
-import numpy as np
-import pandas as pd
-
-data = pd.read_csv("liar_dataset/train.csv", sep=',', header=None, usecols=[1, 2], names=["label", "statement"])
-
-data['label'] = data['label'].apply(lambda x:1 if x in ["false", "pants-fire", "barely-true"] else 0)
-
-print(data)
 
